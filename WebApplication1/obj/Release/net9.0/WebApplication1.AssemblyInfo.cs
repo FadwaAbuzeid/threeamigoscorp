b6c30674a92eebc6f5ffd42507a03a13bdd7fd2b
FadwaAbuzeid <FADWA.OSOS1919@GMAIL.COM>
2024-12-30 06:14:51 +0400

    Add azure publishing config

--- WebApplication1/obj/Release/net9.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Release/net9.0/WebApplication1.AssemblyInfo.cs
@@ -11,9 +11,9 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a707d62108d9982be2249ae800e7e5e5ce502bf")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
